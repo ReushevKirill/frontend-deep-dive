e155b4616f4475d83703595da378d3a9a13e20ac
ReushevKirill <reushev.kd@gmail.com>
2025-06-16 13:08:22 +0300

    feat: added utils: debounce & throttle

---
+++ packages/utils/index.ts
@@ -0,0 +1,46 @@
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  delay: number = 1000
+): (...args: Parameters<T>) => void {
+
+  let timerId: ReturnType<typeof setTimeout>;
+
+  return function(this: ThisParameterType<T>, ...args: Parameters<T>) {
+    const context = this;
+
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+}
+
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  delay: number = 1000
+): (...args: Parameters<T>) => void {
+
+  let isInCooldown = false;
+  let savedArgs: Parameters<T> | null = null;
+  let savedThis: ThisParameterType<T> | null = null;
+
+  return function wrapper(this: ThisParameterType<T>, ...args: Parameters<T>) {
+    if (isInCooldown) {
+      savedArgs = args;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, args);
+    isInCooldown = true;
+
+    setTimeout(() => {
+      isInCooldown = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, delay);
+  };
+}
