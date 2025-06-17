f6e2d3ae89a9ec16f141f391258f5709228b9406
ReushevKirill <reushev.kd@gmail.com>
2025-06-17 18:00:21 +0300

    feat: todo function cloneDeep

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,46 +1,48 @@
 export function debounce<T extends (...args: any[]) => any>(
-  func: T,
-  delay: number = 1000
+	func: T,
+	delay: number = 1000
 ): (...args: Parameters<T>) => void {
+	let timerId: ReturnType<typeof setTimeout>
 
-  let timerId: ReturnType<typeof setTimeout>;
+	return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+		const context = this
 
-  return function(this: ThisParameterType<T>, ...args: Parameters<T>) {
-    const context = this;
+		clearTimeout(timerId)
 
-    clearTimeout(timerId);
-
-    timerId = setTimeout(() => {
-      func.apply(context, args);
-    }, delay);
-  };
+		timerId = setTimeout(() => {
+			func.apply(context, args)
+		}, delay)
+	}
 }
 
 export function throttle<T extends (...args: any[]) => any>(
-  func: T,
-  delay: number = 1000
+	func: T,
+	delay: number = 1000
 ): (...args: Parameters<T>) => void {
+	let isInCooldown = false
+	let savedArgs: Parameters<T> | null = null
+	let savedThis: ThisParameterType<T> | null = null
 
-  let isInCooldown = false;
-  let savedArgs: Parameters<T> | null = null;
-  let savedThis: ThisParameterType<T> | null = null;
+	return function wrapper(this: ThisParameterType<T>, ...args: Parameters<T>) {
+		if (isInCooldown) {
+			savedArgs = args
+			savedThis = this
+			return
+		}
 
-  return function wrapper(this: ThisParameterType<T>, ...args: Parameters<T>) {
-    if (isInCooldown) {
-      savedArgs = args;
-      savedThis = this;
-      return;
-    }
+		func.apply(this, args)
+		isInCooldown = true
 
-    func.apply(this, args);
-    isInCooldown = true;
+		setTimeout(() => {
+			isInCooldown = false
+			if (savedArgs) {
+				wrapper.apply(savedThis!, savedArgs)
+				savedArgs = savedThis = null
+			}
+		}, delay)
+	}
+}
 
-    setTimeout(() => {
-      isInCooldown = false;
-      if (savedArgs) {
-        wrapper.apply(savedThis!, savedArgs);
-        savedArgs = savedThis = null;
-      }
-    }, delay);
-  };
+export function cloneDeep(value: unknown) {
+	// TODO: cloneDeep
 }
