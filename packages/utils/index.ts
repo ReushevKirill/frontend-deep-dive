ff27dba550d27087f2cbb9fa23c4af1725589e3f
Kirill <reushev.kd@gmail.com>
2025-06-19 20:17:51 +0300

    refactor: utils: deepClone

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -81,11 +81,11 @@ function cloneDeepInner<T = unknown>(value: T, visited: Set<T>) {
     Object.entries(value).forEach(([k, v]) => {
       if (v === value) {
         visited.add(v)
-        return
+        copy[k] = v
+      } else {
+        let copiedValue = cloneDeep(v)
+        copy[k] = copiedValue
       }
-
-      let copiedValue = cloneDeep(v)
-      copy[k] = copiedValue
     })
 
     return copy
